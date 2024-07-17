d76a73109261bb39c65c6a30558eceb65bb6049e
vishal <vishalchanda2018@gmail.com>
2024-07-17 12:47:12 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Header } from "./Components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h1 className="mt-5">rfdhuiohfoil</h1>
+    </div>
+  );
+};
